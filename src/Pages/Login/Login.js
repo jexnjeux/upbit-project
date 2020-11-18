9d862c804b5aadf77e4e9f091a61cceb93185aac
eunsu-lim <limes1787@gmail.com>
2020-11-18 13:21:07 +0900

    login, signup 페이지 추가

---
+++ src/Pages/Login/Login.js
@@ -0,0 +1,33 @@
+import React from "react";
+import styled from "styled-components";
+
+function Login() {
+  return (
+    <LoginWrap>
+      <LoginContainer>
+        <LoginContent>
+          <LoginBox>
+            <h2>WEcoin</h2>
+            <p>방문하신 사이트의 주소가 아래와 일치하는지 확인해주세요.</p>
+          </LoginBox>
+        </LoginContent>
+      </LoginContainer>
+    </LoginWrap>
+  );
+}
+
+export default Login;
+
+const LoginWrap = styled.div`
+  background-color: #e9ecf1;
+`;
+
+const LoginContainer = styled.div`
+  margin: 0 auto;
+  width: 532px;
+  background-color: #fff;
+`;
+
+const LoginContent = styled.div``;
+
+const LoginBox = styled.div``;
--- src/Routes.js
+++ src/Routes.js
@@ -1,8 +1,10 @@
-import React, { Component } from 'react';
-import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
-import Nav from '../src/Components/Nav/Nav';
-import Footer from '../src/Components/Footer/Footer';
-import Exchange from './Pages/Exchange/Exchange';
+import React, { Component } from "react";
+import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
+import Nav from "../src/Components/Nav/Nav";
+import Footer from "../src/Components/Footer/Footer";
+import Exchange from "./Pages/Exchange/Exchange";
+import SignUp from "./Pages/SignUp/SignUp";
+import Login from "./Pages/Login/Login";
 
 export default class Routes extends Component {
   render() {
@@ -11,7 +13,9 @@ export default class Routes extends Component {
         <Router>
           <Nav />
           <Switch>
-            <Route exact path='/' component={Exchange} />
+            <Route exact path="/" component={Exchange} />
+            <Route exact path="/signup" component={SignUp} />
+            <Route exact path="/login" component={Login} />
           </Switch>
           <Footer />
         </Router>
