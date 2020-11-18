20dc01acc72b886cc0d4f69c5b6105e524330da1
Juhee Jang <juheejang.dev@gmail.com>
2020-11-18 15:02:21 +0900

    SignUp, SignIn 페이지 추가

---
+++ src/Pages/SignIn/SignIn.js
@@ -0,0 +1,14 @@
+import React from "react";
+import styled from "styled-components";
+
+function SignIn() {
+  return (
+    <SignInWrap>
+      <h1>WeCoin SignIn</h1>
+    </SignInWrap>
+  );
+}
+
+export default SignIn;
+
+const SignInWrap = styled.div``;
--- src/Routes.js
+++ src/Routes.js
@@ -3,6 +3,8 @@ import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
 import Nav from "../src/Components/Nav/Nav";
 import Footer from "../src/Components/Footer/Footer";
 import Exchange from "./Pages/Exchange/Exchange";
+import SignUp from "./Pages/SignUp/SignUp";
+import SingIn from "./Pages/SignIn/SignIn";
 
 export default class Routes extends Component {
   render() {
@@ -12,6 +14,8 @@ export default class Routes extends Component {
           <Nav />
           <Switch>
             <Route exact path="/" component={Exchange} />
+            <Route exact path="/signup" component={SignUp} />
+            <Route exact path="/signin" component={SingIn} />
           </Switch>
           <Footer />
         </Router>
